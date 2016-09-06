4b139312621d770e82aaadd9d8cba2f06914331e
propet <luisdomingoaranda@gmail.com>
2016-09-06 23:15:27 +0200

    aumenta altura de fabrica de particulas, y numero de procesos MPI a determinar

--- ejecuta.py
+++ ejecuta.py
@@ -7,17 +7,17 @@ os.system('sudo mkdir ./post')
 os.system('sudo chmod -R 777 post')
 
 # Comprime hasta llegar a los 8 mm de altura respecto la base de la matriz
-os.system('mpirun -np 4 liggghts < in.plantilla1')
+os.system('mpirun -np * liggghts < in.plantilla1')
 
 # Relaja
-os.system('mpirun -np 4 liggghts < in.plantilla3')
+os.system('mpirun -np * liggghts < in.plantilla3')
 
 # Recupera el ultimo archivo con la configuracion del sistema
 newest = max(glob.iglob('./post/dump*.pruebas'), key=os.path.getctime) # particulas antes de retirar paredes
 os.system('mv %s ./dump1' % (newest))
 
 #Libera
-os.system('mpirun -np 4 liggghts < in.plantilla4')
+os.system('mpirun -np * liggghts < in.plantilla4')
 
 # Recupera el ultimo archivo con la configuracion del sistema
 newest = max(glob.iglob('./post/dump*.pruebas'), key=os.path.getctime) # particulas en reposos sin paredes
